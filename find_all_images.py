8d8187213e7b09c509679fc582eb28d86fd82e69
FARN Forex <FARN Forex>
2016-09-10 17:06:16 -0700

    possible options to get date time included

--- find_all_images.py
+++ find_all_images.py
@@ -1,9 +1,24 @@
 import os
+import sys
 import filecmp
 from datetime import datetime
 from PIL import Image
 from PIL.ExifTags import TAGS
 
+exif_keys = ['LightSource', 'YResolution', 'ResolutionUnit', 'FlashPixVersion',
+             'Make', 'Flash', 'SceneCaptureType', 'DateTime', 'MeteringMode',
+             'XResolution', 'Contrast', 'Saturation', 'MakerNote', 'ExposureProgram',
+             'FocalLengthIn35mmFilm', 'ColorSpace', 'ExifImageWidth',
+             'ExposureBiasValue', 'DateTimeOriginal', 'UserComment',
+             'SceneType', 'Software', 'SubjectDistanceRange', 'WhiteBalance',
+             'CompressedBitsPerPixel', 'DateTimeDigitized', 'SensingMethod',
+             'FNumber', 'CustomRendered', 'FocalLength', 'SubsecTimeOriginal',
+             'ExposureMode', 'ComponentsConfiguration', 'ExifOffset',
+             'ExifImageHeight', 'SubsecTimeDigitized', 'ISOSpeedRatings',
+             'Model', 'Orientation', 'ExposureTime', 'FileSource', 'SubsecTime',
+             'MaxApertureValue', 'ExifInteroperabilityOffset', 'CFAPattern',
+             'Sharpness', 'GainControl', 'YCbCrPositioning', 'DigitalZoomRatio',
+             'ExifVersion']
 # an image is provided
 def getExif(img):
     
@@ -38,6 +53,7 @@ def listAllImages(root):
     
     try:
         #root = r"G:\Pictures" #\house_redlands\verona drive"
+        root = r"C:\temp"
         image_count = 0
         video_count = 0
         
@@ -46,35 +62,45 @@ def listAllImages(root):
             for f in files:
                 
                 file_path = os.path.join(root_folder, f)
+                #print(file_path)
+##                if len(f.split(".")) == 2:
+##                    if f.split(".")[1].lower() in ["avi", "mov", "mpeg", "mpg"]:
+##                        video_count += 1
+##                        #print(file_path)
                 
-                if len(f.split(".")) == 2:
-                    if f.split(".")[1].lower() in ["avi", "mov", "mpeg", "mpg"]:
-                        video_count += 1
-                        #print(file_path)
-                
-                ## check whether it's an image
-                #try:
-                    #img = Image.open(file_path)
-                    #image_count += 1
+                # check whether it's an image
+                try:
+                    img = Image.open(file_path)
+                    image_count += 1
                     
-                    #exif = getExif(img)
-                    
-                    #if exif:
-                        #print("{} : {}".format(f, exif['DateTimeOriginal']))
-                              
-                    #else:
-                        ## look for os.stat time information
-                        #modifiedtime = datetime.fromtimestamp(os.path.getmtime(file_path))
-                        #if modifiedtime:
-                            #image_count += 1
-                            #print("Modified time: ", modifiedtime.year, modifiedtime.month, modifiedtime.day)
-                        #else:
-                            #print("{} has no timestamp")
-                #except:
-                    #print("NOT AN IMAGE")
+                    exif = getExif(img)
+
+                    if exif:
+                        #print("Date time original - {} : {}".format(f, exif['DateTimeOriginal']))
+                        #print("Date Taken - {} : {}".format(f, exif['DateTime']))  # WRONG WRONG
+                        print("DateTaken - {} and Make - {}".format(exif['DateTime'], exif['Make']))
+                    else:
+                        # look for os.stat time information
+                        modifiedtime = datetime.fromtimestamp(os.path.getmtime(file_path))
+                        if modifiedtime:
+                            image_count += 1
+                            print("Modified time: ", modifiedtime.year, modifiedtime.month, modifiedtime.day)
+                        else:
+                            print("{} has no timestamp")
+                            
+                except Exception as ex0:
+                    ext = os.path.splitext(file_path)[1].lower()
+                    if ext in [".jpg", ".jpeg", ".png"]:
+                        modifiedtime = datetime.fromtimestamp(os.path.getmtime(file_path))
+                        print("{}: {}".format(file_path, modifiedtime))
+                    else:
+                        print("NOT AN IMAGE: {}".format(file_path))
+                        
+                except Exception as ex:
+                    print(ex.args[0])
                        
         #print("{} : is total number of images.".format(image_count))
-        print("{} : is total number of videos.".format(video_count))  
+        #print("{} : is total number of videos.".format(video_count))  
 
     except Exception as ex:
         print(ex.args[0])
@@ -82,8 +108,12 @@ def listAllImages(root):
         
 
 if __name__ == '__main__':
-    
-    listAllImages(r"G:\Pictures")
+
+    root = r"C:\Users\farnf\OneDrive\Pictures\FromFujiCamera"
+    root = r"C:\temp"
+    root = r"C:\Users\farnf\Pictures\2016-07"
+    root = r"D:\DCIM\101_FUJI"
+    listAllImages(root)
     #try:
         #root = r"G:\Pictures" #\house_redlands\verona drive"
         #image_count = 0
