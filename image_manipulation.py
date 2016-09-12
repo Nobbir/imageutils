184c25bec64e52098d34dad67e1fcb137a52b927
nobbir <nobbir@gmail.com>
2016-09-12 09:31:21 -0700

    Contains defs for image manipulations

--- image_manipulation.py
+++ image_manipulation.py
@@ -7,42 +7,40 @@ from PIL.ExifTags import TAGS
 from PIL import ImageEnhance
 
 
-exif_keys = ['LightSource', 'YResolution', 'ResolutionUnit', 'FlashPixVersion',
-             'Make', 'Flash', 'SceneCaptureType', 'DateTime', 'MeteringMode',
-             'XResolution', 'Contrast', 'Saturation', 'MakerNote', 'ExposureProgram',
-             'FocalLengthIn35mmFilm', 'ColorSpace', 'ExifImageWidth',
-             'ExposureBiasValue', 'DateTimeOriginal', 'UserComment',
-             'SceneType', 'Software', 'SubjectDistanceRange', 'WhiteBalance',
-             'CompressedBitsPerPixel', 'DateTimeDigitized', 'SensingMethod',
-             'FNumber', 'CustomRendered', 'FocalLength', 'SubsecTimeOriginal',
-             'ExposureMode', 'ComponentsConfiguration', 'ExifOffset',
-             'ExifImageHeight', 'SubsecTimeDigitized', 'ISOSpeedRatings',
-             'Model', 'Orientation', 'ExposureTime', 'FileSource', 'SubsecTime',
-             'MaxApertureValue', 'ExifInteroperabilityOffset', 'CFAPattern',
-             'Sharpness', 'GainControl', 'YCbCrPositioning', 'DigitalZoomRatio',
-             'ExifVersion']
+def image_difference(im1, im2):
+    out = abs(image1, image2)  # returns an image ...
+    return out
 
+print("stop")
 
-# image path is provided
-def get_exif(img_path):
+def saveImageAsGrayscale(image):
+    image.save("L")
+        
+#try:
+    ##filename, ext = os.path.splitext(imfile1)
+    ##new_file = filename + "_mini" + '.JPEG'
+    ##size = (128, 128)
+    #im1= Image.open(imfile1)
+    ##im1.thumbnail(size, Image.ANTIALIAS)
+    ##im.save(new_file, 'JPEG')
     
-    ret = {}
-    try:
-        iv = Image.open(img_path)
-        info = iv._getexif()
-        for tag, value in info.items():
-            decoded = TAGS.get(tag, value)
-            ret[decoded] = value
-        return ret
+    ##filename, ext = os.path.splitext(imfile2)
+    ##new_file = filename + "_mini" + '.JPEG'
+    #im2= Image.open(imfile2)
+    ##im2.thumbnail(size, Image.ANTIALIAS)
 
-    except Exception as ex:
-        print("Image {} does not have any exif".format(img_path))
-        # use os.stat(image_path) to get time info
-        return
-        
+    ##d = ImageChops.difference(im1, im2)
+    ##if d.getbbox() is None:
+        ##print("No difference")
+    ##else:
+        ##d.save(r'C:\temp\thumbdiff.jpg', 'JPEG')
 
-        
-
+    #print(timeit.timeit(stmt=a, number=100000))
+    #print(timeit.timeit(stmt=b, number=100000))
+    
+#except Exception as ex:
+    #print(ex.args[0])
+    
 if __name__ == '__main__':
 
     sample_image = os.path.join(os.getcwd(), "rubyct.JPG")
