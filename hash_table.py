ceeac7a1d1336c128ad745a9aee000cf2df6c7f4
nobbir <nobbir@gmail.com>
2016-09-14 09:17:07 -0700

    Experiments with 2-3 pixels differing images

--- hash_table.py
+++ hash_table.py
@@ -13,6 +13,8 @@ import hashlib
 # http://blog.iconfinder.com/detecting-duplicate-images-using-python/
 pic1 = r"C:\Users\nobi4775\Pictures\GooglePhonePicturesDhaka\IMG_20130331_005901.jpg"
 pic2 = r"C:\Users\nobi4775\Pictures\GooglePhonePicturesDhaka\IMG_20130331_005906.jpg"
+pic3 = r"C:\Users\nobi4775\Pictures\GooglePhonePicturesDhaka\IMG_20130331_005901.jpg"
+pic4 = r"C:\Users\nobi4775\Pictures\GooglePhonePicturesDhaka\IMG_20130331_005901Copy.jpg"   # 5-6 pixel is different
 
 ###
 ## hash table - Image cataloging ....
@@ -26,5 +28,12 @@ hash1 = hashlib.md5(image_file).hexdigest()
 image_file_modified = open(pic2).read()
 hash2 = hashlib.md5(image_file_modified).hexdigest()
 
+image_file = open(pic3).read()
+hash3 = hashlib.md5(image_file).hexdigest()
+
+image_file = open(pic4).read()
+hash4 = hashlib.md5(image_file).hexdigest()
+
+print(hash1 == hash4)
 print(hash1)
-print(hash2)
+print(hash3)
