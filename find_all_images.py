94bf97f654375f8e5a7bc0d6fe77e539cbfd5c7a
nobbir <nobbir@gmail.com>
2016-09-15 16:18:06 -0700

    Date functions are brought in. This script should be the main

--- find_all_images.py
+++ find_all_images.py
@@ -21,19 +21,15 @@ exif_keys = ['LightSource', 'YResolution', 'ResolutionUnit', 'FlashPixVersion',
              'ExifVersion']
 # an image is provided
 def getExif(img):
-    
     ret = {}
-    
     info = img._getexif()
     for tag, value in info.items():
         decoded = TAGS.get(tag, value)
         ret[decoded] = value
-        
     return ret
 
 # image path is provided
 def get_exif(img_path):
-    
     ret = {}
     try:
         iv = Image.open(img_path)
@@ -42,13 +38,46 @@ def get_exif(img_path):
             decoded = TAGS.get(tag, value)
             ret[decoded] = value
         return ret
-
     except Exception as ex:
         print("Image {} does not have any exif".format(img_path))
         # use os.stat(image_path) to get time info
         return
         
         
+def ConvertTimestampToLong(date_str):  # e.g., '2010:07:14 20:43:00'
+    
+    dt = date_str.split()[0]
+    tm = date_str.split()[1]
+    year, month, day = dt.split(":")
+    year = int(year.strip())
+    month = int(month.strip())
+    day = int(day.strip())
+
+    hour, minute, second = tm.split(":")
+    hour = int(hour.strip())
+    minute = int(minute.strip())
+    second = int(second.strip())
+
+    return second * 1 + minute * 100 + hour * 10000 + day * 1000000 + month * 100000000 + year * 10000000000
+
+    
+def ConvertTimestampToDateTime(date_str): # e.g., '2010:07:14 20:43:00'
+
+    dt = date_str.split()[0]
+    tm = date_str.split()[1]
+    year, month, day = dt.split(":")
+    year = int(year.strip())
+    month = int(month.strip())
+    day = int(day.strip())
+
+    hour, minute, second = tm.split(":")
+    hour = int(hour.strip())
+    minute = int(minute.strip())
+    second = int(second.strip())
+
+    return year, month, day, hour, minute, second
+
+
 def listAllImages(root):
     
     try:
@@ -61,7 +90,7 @@ def listAllImages(root):
             
             for f in files:
                 
-                file_path = os.path.join(root_folder, f)
+                file_path = os.path.join(root_folder, f)   # f is file name, e.g., AM234.jpg
                        
                 ext = os.path.splitext(file_path)[1].lower()
                 
@@ -69,8 +98,10 @@ def listAllImages(root):
                     continue
                 
                 if ext in [".avi", ".mov", ".mpeg", ".mpg", ".mp4"]:
-                    modifiedtime = datetime.fromtimestamp(os.path.getmtime(file_path))
-                    print("{}: {}".format(file_path, modifiedtime))                    
+                    #consider only images for now ------------------
+                    #modifiedtime = datetime.fromtimestamp(os.path.getmtime(file_path))
+                    #print("{}: {}".format(file_path, modifiedtime))    
+                    continue
                 
                 # check whether it's an image
                 try:
@@ -80,12 +111,13 @@ def listAllImages(root):
                     exif = getExif(img)
 
                     if exif:
-                        #print("Date time original - {} : {}".format(f, exif['DateTimeOriginal']))
-                        #print("Date Taken - {} : {}".format(f, exif['DateTime']))  # WRONG WRONG
+                        date_time_org = ConvertTimestampToDateTime(exif['DateTimeOriginal'])
+                        date_time_taken = ConvertTimestampToDateTime(exif['DateTime'])   # this is the one work FIRST
+                        camera = exif['Make']
                         print("DateTaken - {} and Make - {}".format(exif['DateTime'], exif['Make']))
                     else:
                         # look for os.stat time information
-                        modifiedtime = datetime.fromtimestamp(os.path.getmtime(file_path))
+                        modifiedtime = datetime.fromtimestamp(os.path.getmtime(file_path))   # returns a time string 
                         if modifiedtime:
                             image_count += 1
                             print("Modified time: ", modifiedtime.year, modifiedtime.month, modifiedtime.day)
@@ -95,7 +127,7 @@ def listAllImages(root):
                 except Exception as ex0:
                     
                     if ext in [".jpg", ".jpeg", ".png"]:
-                        modifiedtime = datetime.fromtimestamp(os.path.getmtime(file_path))
+                        modifiedtime = datetime.fromtimestamp(os.path.getmtime(file_path))   # returns a DateTime object
                         print("{}: {}".format(file_path, modifiedtime))
                     else:
                         print("NOT AN IMAGE: {}".format(file_path))
@@ -110,6 +142,20 @@ def listAllImages(root):
         
 
 if __name__ == '__main__':
+    """
+    Searching Python list of dictionaries: http://stackoverflow.com/questions/8653516/python-list-of-dictionaries-search
+    user generator expression : https://www.python.org/dev/peps/pep-0289/
+    >>> dicts = [
+    ...     { "name": "Tom", "age": 10 },
+    ...     { "name": "Mark", "age": 5 },
+    ...     { "name": "Pam", "age": 7 },
+    ...     { "name": "Dick", "age": 12 }
+    ... ]
+    
+    >>> (item for item in dicts if item["name"] == "Pam").next()
+    {'age': 7, 'name': 'Pam'}
+    
+    """
 
     root = r"C:\Users\farnf\OneDrive\Pictures\FromFujiCamera"
     root = r"C:\temp"
