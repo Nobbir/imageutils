8b240945b6e84c8b197041645dd8fc8d2625b8c7
nobbir <nobbir@gmail.com>
2016-09-15 15:56:50 -0700

    Thumbnail experiments

--- image_manipulation.py
+++ image_manipulation.py
@@ -56,9 +56,6 @@ def ImageChops():
         print(ex.args[0])
 
 def ImageResize(image_path):
-
-    image_path = "rubyct.JPG"
-    
     try:
         with Image.open(image_path) as image:
             im2 = image.copy()
@@ -67,6 +64,46 @@ def ImageResize(image_path):
             small = im2.resize((width/100, height/100))
             small.save("small.jpg")
             
+    except Exception as ex:
+        print(ex.args[0])
+        
+        
+def ImageResizeCentered(image_path):
+    try:
+        with Image.open(image_path) as image:
+            im2 = image.copy()
+            width, height = im2.size
+            im3 = im2.convert("L") #.save("grayscale2.jpg")
+            
+            im3.thumbnail((256, 256))
+            im3.save("thumbnail_gray2.jpg", 'JPEG')
+            
+            #centerx = width / 2
+            #centery = height / 2
+            
+            #diff = 256
+            #ulx = centerx - diff
+            #uly = centery - diff
+            #lrx = centerx + diff
+            #lry = centery + diff
+            
+            ## pass an extent x0, y0, x1, y1 to Image.crop method
+            #centered = im3.crop((ulx, uly, lrx, lry))
+            #centered.save("centered2.jpg")
+            
+    except Exception as ex:
+        print(ex.args[0])
+        
+    
+def ResizeExperiments(image_path, size=(128, 128)):
+    try:       
+        file_path, file_name = os.path.split(image_path)
+        filename, ext = os.path.splitext(file_name)
+        thumbnail_name = filename + "_{}".format(size[0]) + ext        
+
+        with Image.open(image_path) as image:
+            image.thumbnail(size)
+            image.save(thumbnail_name, 'JPEG')
     except Exception as ex:
         print(ex.args[0])
     
@@ -90,6 +127,6 @@ if __name__ == '__main__':
     image_path = os.path.join(os.getcwd(), "rubyct.JPG")
 
     #SaveImageAsThumnail(image_path)
-    
+    #ResizeExperiments(image_path, (100, 100))
     #saveImageAsGrayscale(image_path)
-    ImageResize(image_path)
+    ImageResizeCentered(image_path)
