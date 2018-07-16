3139a85feb42a03ffe0c6a822c0451bc80d024ff
nobbir <nobbir@gmail.com>
2018-07-15 18:24:16 -0700

    parseDateTime(...) moved to image_utilities.py

--- catalog_by_date.py
+++ catalog_by_date.py
@@ -31,59 +31,7 @@ exif date time is returned as a string: '2016:08:01 22:12:20' - this is converte
 
 ###x = sorted([dt1, dt2, dt3, dt4])
 
-# input '2016:08:01 22:12:20'
-# output: a tuple of 6 numbers
-def parseDateTime(dt_string):
-    
-    if not dt_string: return
-    
-    _date = dt_string.split()[0]
-    _time = dt_string.split()[1]
-    
-    d = _date.split(":")
-    t = _time.split(":")
-    
-    year = int(d[0].strip())
-    month = int(d[1].strip())
-    day = int(d[2].strip())
-    hour = int(t[0].strip())
-    minute = int(t[1].strip())
-    second = int(t[2].strip())
-    
-    return [year, month, day, hour, minute, second]
-        
 
-def getExif(img_path):
-    ret = {}
-    try:
-        im = Image.open(img_path)
-        info = im._getexif()
-        for tag, value in info.items():
-            decoded = TAGS.get(tag, value)
-            ret[decoded] = value
-        return ret
-    except Exception as ex:
-        print(ex.args[0])
-        return
-
-
-pic = r"C:\Users\nobi4775\Pictures\amelia_fokla_IMG_2381.JPG"
-pic2 = r"C:\Users\nobi4775\Pictures\Eid2015_Fab_Me_Am.jpg"    #r'C:\Users\nobi4775\Pictures\me_savar_abedin_home2'
-exif1 = getExif(pic)
-exif2 = getExif(pic2)
-
-if exif1 and 'DateTimeOriginal' in exif1.keys():
-    date1 = parseDateTime(exif1['DateTimeOriginal'])
-else:
-    date1 = None
-    
-if exif2 and 'DateTimeOriginal' in exif2.keys():
-    date2 = parseDateTime(exif2['DateTimeOriginal'])
-else:
-    date2 = None
-    
-if date1 and date2:
-    print(testListEquality(date1, date2))
     
 ####################
 location = r"C:\Users\nobi4775\Pictures"
--- image_utilities.py
+++ image_utilities.py
@@ -8,14 +8,55 @@ from PIL import ImageChops
 from PIL import Image
 from PIL import ImageStat
 
+
 exif_tags = ['CompressedBitsPerPixel', 'DateTimeOriginal', 'DateTimeDigitized', 'MaxApertureValue', 'MeteringMode',
              'LightSource', 'FocalLength', 'ExifImageWidth', 'Make', 'Model', 'SubsecTimeOriginal', 'Orientation',
              'YCbCrPositioning', 'SensingMethod', 'XResolution', 'YResolution', 'ExposureTime', 'ISOSpeedRatings',
              'FNumber', 'DateTime', 'ExifImageHeight']
 
+# input '2016:08:01 22:12:20'
+# output: a tuple of 6 numbers
+def parseDateTime(dt_string):
+    
+    if not dt_string: return
+    
+    _date = dt_string.split()[0]
+    _time = dt_string.split()[1]
+    
+    d = _date.split(":")
+    t = _time.split(":")
+    
+    year = int(d[0].strip())
+    month = int(d[1].strip())
+    day = int(d[2].strip())
+    hour = int(t[0].strip())
+    minute = int(t[1].strip())
+    second = int(t[2].strip())
+    
+    return [year, month, day, hour, minute, second]
+
+
 # image path is provided
 def get_exif(img_path):
-    
+    """
+    pic = r"C:\Users\nobi4775\Pictures\amelia_fokla_IMG_2381.JPG"
+    pic2 = r"C:\Users\nobi4775\Pictures\Eid2015_Fab_Me_Am.jpg"    #r'C:\Users\nobi4775\Pictures\me_savar_abedin_home2'
+    exif1 = getExif(pic)
+    exif2 = getExif(pic2)
+
+    if exif1 and 'DateTimeOriginal' in exif1.keys():
+        date1 = parseDateTime(exif1['DateTimeOriginal'])
+    else:
+        date1 = None
+        
+    if exif2 and 'DateTimeOriginal' in exif2.keys():
+        date2 = parseDateTime(exif2['DateTimeOriginal'])
+    else:
+        date2 = None
+        
+    if date1 and date2:
+        print(testListEquality(date1, date2))
+    """
     ret = {}
     try:
         iv = Image.open(img_path)
@@ -28,6 +69,7 @@ def get_exif(img_path):
     except Exception as ex:
         print("{} does not have any exif".format(img_path))
         # use os.stat(image_path) to get time info
+        print(ex.args[0])
         return
     
     
