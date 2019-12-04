80b5c6d4823dcb40e70516274fa0b6ae67b1e4d6
Nobbir <nobbir@gmail.com>
2019-12-04 12:26:39 -0800

    new script for comparing by hash values

--- hash_table.py
+++ hash_table.py
@@ -1,23 +1,26 @@
 import filecmp
 
-from PIL import Image
-from PIL.ExifTags import TAGS
+##from PIL import Image
+##from PIL.ExifTags import TAGS
 import sys
 import os
-from PIL import ImageChops
-from PIL import Image
-from PIL import ImageStat
+##from PIL import ImageChops
+##from PIL import Image
+##from PIL import ImageStat
 import hashlib
 
-
-# http://blog.iconfinder.com/detecting-duplicate-images-using-python/
+#http://blog.iconfinder.com/detecting-duplicate-images-using-python/
 pic1 = r"C:\Users\nobi4775\Pictures\GooglePhonePicturesDhaka\IMG_20130331_005901.jpg"
 pic2 = r"C:\Users\nobi4775\Pictures\GooglePhonePicturesDhaka\IMG_20130331_005906.jpg"
 pic3 = r"C:\Users\nobi4775\Pictures\GooglePhonePicturesDhaka\IMG_20130331_005901.jpg"
 pic4 = r"C:\Users\nobi4775\Pictures\GooglePhonePicturesDhaka\IMG_20130331_005901Copy.jpg"   # 5-6 pixel is different
+pic1 = r"C:\Users\nobi4775\Pictures\MinecraftCake\200_s2.png"
+pic2 = r"C:\Users\nobi4775\Pictures\MinecraftCake\200_s1.gif"
+pic3 = r"C:\Users\nobi4775\Pictures\MinecraftCake\1403995931-wild-sheep.png"
+pic4 = r"C:\Users\nobi4775\Pictures\MinecraftCake\200_s.gif"
 
 ###
-## hash table - Image cataloging ....
+##  hash table - Image cataloging ....
 ### 
 
 # original image's hash value
@@ -34,6 +37,7 @@ hash3 = hashlib.md5(image_file).hexdigest()
 image_file = open(pic4).read()
 hash4 = hashlib.md5(image_file).hexdigest()
 
-print(hash1 == hash4)
-print(hash1)
+print("200_s2 png  : {}".format(hash1))
+print("200_s2 png  : {}".format(hash1))
 print(hash3)
+print(hash4)
